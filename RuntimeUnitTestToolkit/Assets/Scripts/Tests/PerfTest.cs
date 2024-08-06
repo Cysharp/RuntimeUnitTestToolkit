56103fb4b8c3825293f940fa9a73195b2a6168e0
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-08-06 15:10:45 +0900

    chore: add Performance Test to confirm test fail

---
+++ RuntimeUnitTestToolkit/Assets/Scripts/Tests/PerfTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Unity.PerformanceTesting;
+using UnityEngine;
+
+namespace SampleUnitTest
+{
+    public class PerfTest
+    {
+        AnotherClass another;
+        string anotherJson;
+
+        public PerfTest()
+        {
+            another = new AnotherClass();
+            anotherJson = JsonUtility.ToJson(another);
+        }
+
+        [Test, Performance]
+        public void DummySerialize()
+        {
+            Measure.Method(() =>
+            {
+                JsonUtility.ToJson(another);
+            })
+            .WarmupCount(10)
+            .IterationsPerMeasurement(10000)
+            .MeasurementCount(10)
+            .Run();
+        }
+
+        [Test, Performance]
+        public void DummyDeserialize()
+        {
+            Measure.Method(() =>
+            {
+                JsonUtility.FromJson<AnotherClass>(anotherJson);
+            })
+            .WarmupCount(10)
+            .IterationsPerMeasurement(10000)
+            .MeasurementCount(10)
+            .Run();
+        }
+    }
+}
